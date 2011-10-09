f020282ca333e7c26cd8f979afe62bd53302d69b
mikhaylova.alexandra.a <mikhaylova.alexandra.a@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-10-09 22:20:07 +0000

    Prime numbers

---
+++ trunk/Alexandra_Mikhaylova/testProj/testProj/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 100;
+            int[] nums = new int[N+1];
+            for (int i = 0; i < N + 1; i ++)
+                nums[i] = 0;
+            for (int i = 2; i < N + 1; i ++)
+                {
+                    if (nums[i] == 1)
+                        continue;
+                    for (int k = 2; i * k < N + 1; k++)
+                        nums[i * k] = 1;
+                }
+
+            for (int i = 2; i < N + 1; i++)
+                if (nums[i] == 0)
+                    Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
