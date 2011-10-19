62533a1fbe19924f096ba4f1f95c4d20a8edec82
FWFKirillPopov@gmail.com <FWFKirillPopov@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-10-19 13:51:25 +0000

    Session/SessionManager.cs was added.

---
+++ SkyShoot/SkyShootService/Session/SessionManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkyShoot.Contracts.Session;
+
+namespace SkyShootService.Session
+{
+    public class SessionManager
+    {
+        private List<GameDescription> GameDescriptions;
+
+        private int GameID;
+
+        public SessionManager()
+        {
+            GameDescriptions = new List<GameDescription>();
+            GameID = 1;
+        }
+
+        public bool JoinGame(GameDescription game, string PlayerName)
+        {
+            try
+            {
+                game.Players[game.Players.Length] = PlayerName;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public GameDescription CreateGame(GameMode mode, int maxPlayers, string playerName)
+        {
+            /* IMHO лучше использовать List<string>, чем string[]. Т.к. это очень сильно 
+             * упростит программирование и удаление отключившихся игроков.
+             */
+
+            string[] PlayerNames;
+            PlayerNames = new string[maxPlayers];
+            PlayerNames[0] = playerName;
+
+            var gameDescription = new GameDescription(PlayerNames, maxPlayers, mode, GameID++);
+            GameDescriptions.Add(gameDescription);
+
+            return gameDescription;
+        }
+
+        public GameDescription[] GetGameList()
+        {
+            return GameDescriptions.ToArray();
+        }
+    }
+}
