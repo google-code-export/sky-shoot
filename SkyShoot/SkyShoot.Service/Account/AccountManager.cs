e0d5cac892fde75365023dd58d9557d978dbfaaa
Sergey.Tereshchenko.spbgu@gmail.com <Sergey.Tereshchenko.spbgu@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-10-25 19:15:02 +0000

    AccountManager(old ver.), HW was added

---
+++ SkyShoot/SkyShoot.Service/Account/AccountManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkyShoot.Service.Account
+{
+    public class AccountManager
+    {
+        private string line;
+
+        private bool LoginBefore(string Login)
+        {
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"пока не понтяно"); // будем переделывать
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Split(' ')[0] == Login)
+                    return true;
+                break;
+            }
+
+            file.Close();
+            return false;
+
+        }
+
+        public bool Register(string username, string password)
+        {
+            if (LoginBefore(username))
+                return false; // такой username уже занят
+            else
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"куда-то", true)) // всё равно будем переделывать
+                {
+                    file.WriteLine(username + " " + password);
+                }
+            return true; // всё ок
+
+        }
+
+        public bool Login(string username, string password)
+        {
+            if (LoginBefore(username))
+                if (password == line.Split(' ')[1])
+                    return true;  // всё ок
+                else
+                    return false; // пароль неверный
+            else
+                return false; // такого логина нет
+
+        }
+    }
+}
