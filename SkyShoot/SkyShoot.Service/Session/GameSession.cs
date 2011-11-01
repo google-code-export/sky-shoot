1f4b10dec4d9322f143eb0aa7f514fb1f934575a
FWFKirillPopov@gmail.com <FWFKirillPopov@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-01 14:48:39 +0000

    Fixed code style. Overwrote names of private variable

--- SkyShoot/SkyShoot.Service/Session/GameSession.cs
+++ SkyShoot/SkyShoot.Service/Session/GameSession.cs
@@ -9,14 +9,14 @@ namespace SkyShoot.Service.Session
 {
     public class GameSession
     {
-		private GameLevel GameLevel;
+		private GameLevel _gameLevel;
 		private List<AMob> Mobs;
 
 		public GameDescription LocalGameDescription { get; private set; }
 
 		public GameSession(TileSet tileSet,List<string> players, int maxPlayersAllowed, GameMode gameType, int gameID)
         {
-			GameLevel = new GameLevel(tileSet);
+			_gameLevel = new GameLevel(tileSet);
 
 			LocalGameDescription = new GameDescription(players, maxPlayersAllowed, gameType, gameID);
 
