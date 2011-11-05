bfea7ab3687bc198d8b70b79ee2f7d563460d47b
Sergey.Tereshchenko.spbgu@gmail.com <Sergey.Tereshchenko.spbgu@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-05 14:08:56 +0000

    AccountManager (Azure Storage Table) was added

--- trunk/SkyShoot/SkyShoot.Service/Account/AccountManager.cs
+++ trunk/SkyShoot/SkyShoot.Service/Account/AccountManager.cs
@@ -2,53 +2,125 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data.Services.Client;
+
 
 namespace SkyShoot.Service.Account
 {
     public class AccountManager
     {
-        private string line;
+        // для теста
+        // Account name: devstoreaccount1
+        // Account key: Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==
+        // прописывается в Web.config
 
-        private bool LoginBefore(string Login)
-        {
-            System.IO.StreamReader file =
-                new System.IO.StreamReader(@"пока не понтяно"); // будем переделывать
+        string storageConnectionString = ConfigurationManager.ConnectionStrings["Storage"].ConnectionString;
+        TableHelper TableHelper = new TableHelper(storageConnectionString);
 
-            while ((line = file.ReadLine()) != null)
-            {
-                if (line.Split(' ')[0] == Login)
-                    return true;
-                break;
-            }
+        /**
+         в начале ещё нужно будет создать таблицу:
+         TableHelper.CreateTable("AccountsTable")
 
-            file.Close();
-            return false;
-
-        }
+        (!) нужно будет делать регистрацию приводя все знаки username / password к строчным
+        ещё проверять нужно будет пароли и имена, чтобы они содержали только a-z,A-Z,1-9 ,-,_ и никаких пробелов
+        **/
 
         public bool Register(string username, string password)
         {
-            if (LoginBefore(username))
-                return false; // такой username уже занят
-            else
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"куда-то", true)) // всё равно будем переделывать
+            Account account = null;
+            if (TableHelper.GetEntity<Account>("AccountsTable", "account", username, out account))
+            {
+                if (account != null)
                 {
-                    file.WriteLine(username + " " + password);
+                    return false; // такой аккаунт уже есть > выберете другое имя для аккаунта
                 }
-            return true; // всё ок
+                else
+                {
+                    if (TableHelper.InsertEntity("AccountsTable",
+                    new Account("account", username) { Login = username, Password = password, Email = "--", Info = "--" })) // пока Email и Info будут пустовать 
+                    {
+                        return true; // регистрация прошла успешно!
+                    }
+                    else
+                    {
+                        return false; // какие-то ошибки вылезли
+                    }
 
+                }
+            }
+            else
+            {
+                return false; // какие-то ошибки вылезли
+            }
         }
 
         public bool Login(string username, string password)
         {
-            if (LoginBefore(username))
-                if (password == line.Split(' ')[1])
-                    return true;  // всё ок
+            Account account = null;
+            if (TableHelper.GetEntity<Account>("AccountsTable", "account", username, out account))
+            {
+                if (account != null)
+                {
+                    if (account.Password == password)
+                    {
+                        return true; // залогинились
+                    }
+                    else
+                    {
+                        return false; // неверный пароль от аккаунта
+                    }
+                }
                 else
-                    return false; // пароль неверный
+                {
+                    return false; // такого аккаунта вообще нет!
+                }
+            }
             else
-                return false; // такого логина нет
-
+            {
+                return false; // какие-то ошибки вылезли
+            }
         }
+
+        public bool CreatePassword(string username, string password, string new_password)
+            // даже если игрок залогинен, то ему для подтверждения нужно вводить заново свои данные (будем это считать зачатками безопасности)
+        {
+            if (Login(username, password))
+            {
+                if (TableHelper.ReplaceUpdateEntity("AccountTable", "account", username,
+                    new Account("account", username) { Password = new_password }))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false; // нельзя менять пароль если не знаешь прошлый
+            }
+        }
+
+        public bool DeleteAccount(string username, string password)
+        {
+            if (Login(username, password)) // надо убедиться что происходит осмысленное удаление аккаунта
+            {
+                if (TableHelper.DeleteEntity<Account>("AccountsTable", "account", username))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false; // проверку не прошёл > удалить аккаунт нельзя
+            }
+        }
+
     }
 }
