b72802b287c299c99e5f04736cf4f288dc1e501c
chuvashov.andrey@gmail.com <chuvashov.andrey@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-06 17:00:04 +0000

    add Logger

---
+++ trunk/SkyShoot/SkyShoot.Service/Logger/DataServiceContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.WindowsAzure;
+
+namespace SkyShoot.Service.Logger
+{
+    class DataServiceContext : TableServiceContext
+    {
+        public DataServiceContext(string baseAddress, StorageCredentials credentials)
+            : base(baseAddress, credentials)
+        {
+
+        }
+
+        public IQueryable<Record> Record
+        {
+            get
+            {
+                return this.CreateQuery<Record>("Records");
+            }
+        }
+
+        public void AddRecord(string message, string level)
+        {
+            this.AddObject("Records", new Record { Message = message, Level = level });
+            this.SaveChanges();
+        }
+
+    }
+}
---
+++ trunk/SkyShoot/SkyShoot.Service/Logger/TableTraceListener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+using Microsoft.WindowsAzure.StorageClient;
+using System.Data.Services.Client;
+using Microsoft.WindowsAzure;
+
+namespace SkyShoot.Service.Logger
+{
+    public class TableTraceListener : TraceListener
+    {
+        CloudStorageAccount account;
+        DataServiceContext context;
+
+        public TableTraceListener()
+        {
+            account = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+            context = new DataServiceContext(account.TableEndpoint.ToString(), account.Credentials);
+
+            CloudTableClient.CreateTablesFromModel(typeof(DataServiceContext),
+                                    account.TableEndpoint.AbsoluteUri, account.Credentials);
+        }
+
+
+        public override void WriteLine(string message)
+        {
+            try
+            {
+                context.AddRecord(message, "info");
+            }
+            catch (DataServiceRequestException)
+            {
+
+            }
+        }
+
+        public override void Fail(string message)
+        {
+            try
+            {
+                context.AddRecord(message, "error");
+            }
+            catch (DataServiceRequestException)
+            {
+
+            }
+        }
+
+        public override void Write(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
