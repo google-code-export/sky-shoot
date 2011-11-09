76f99af829897756fff2eca39a25ef1052670bd8
chuvashov.andrey@gmail.com <chuvashov.andrey@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-09 16:56:31 +0000

    Added beta version of MobFactory.

--- trunk/SkyShoot/SkyShoot.Contracts/Session/GameLevel.cs
+++ trunk/SkyShoot/SkyShoot.Contracts/Session/GameLevel.cs
@@ -14,7 +14,7 @@ namespace SkyShoot.Contracts.Session
 		public const int MOVEMENTTIME = 100; //in milliseconds
 		public float levelHeight = 1000; 
 		public float levelWidth = 1000;
-		public const float LEVELBORDER = 50;
+		public float LEVELBORDER = 50;
 		
         public GameLevel(TileSet usedTileSet)
         {
---
+++ trunk/SkyShoot/SkyShoot.Service/Mobs/Mob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyShoot.Contracts.Mobs
+{
+    public class Mob : AMob
+    {
+        public string targetPlayer { get; set; }
+
+        public Mob()
+        {
+            IsPlayer = false;
+        }
+    }
+}
