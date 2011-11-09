46ff5823a7d23ba81b562d472dc6866d66c492f4
rrevenantt@gmail.com <rrevenantt@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-09 17:36:00 +0000

    Added update function in GameSession

--- trunk/SkyShoot/SkyShoot.Contracts/Session/GameLevel.cs
+++ trunk/SkyShoot/SkyShoot.Contracts/Session/GameLevel.cs
@@ -11,7 +11,7 @@ namespace SkyShoot.Contracts.Session
     [DataContract]
     public class GameLevel
     {
-		public const int MOVEMENTTIME = 100; //in milliseconds
+		//public const int MOVEMENTTIME = 100; //in milliseconds
 		public float levelHeight = 1000; 
 		public float levelWidth = 1000;
 		public float LEVELBORDER = 50;
@@ -26,23 +26,7 @@ namespace SkyShoot.Contracts.Session
             
         }
 
-		public Vector2 ComputeMovement(AMob mob)
-		{
-			var realHeight=levelHeight-mob.Radius;
-			var realWidth=levelWidth-mob.Radius;
-			var newCoord = new Vector2(mob.Coordinates.X + mob.Speed * MOVEMENTTIME * mob.RunVector.X, mob.Coordinates.Y + mob.Speed * MOVEMENTTIME * mob.RunVector.Y);
-			if(Math.Abs(mob.Coordinates.X) <= realWidth)
-				newCoord.X=Math.Min(Math.Abs(newCoord.X), realWidth) * Math.Sign(newCoord.X);
-			else
-				newCoord.X=Math.Min(Math.Abs(newCoord.X), realWidth+LEVELBORDER) * Math.Sign(newCoord.X);
-				
-			if (Math.Abs(mob.Coordinates.Y) <= realHeight)
-				newCoord.Y = Math.Min(Math.Abs(newCoord.Y), realHeight) * Math.Sign(newCoord.Y);
-			else
-				newCoord.Y = Math.Min(Math.Abs(newCoord.Y), realHeight + LEVELBORDER) * Math.Sign(newCoord.Y);
-
-			return newCoord;
-		}
+		
 
         [DataMember]
         public TileSet UsedTileSet
--- trunk/SkyShoot/SkyShoot.Service/Session/GameSession.cs
+++ trunk/SkyShoot/SkyShoot.Service/Session/GameSession.cs
@@ -6,35 +6,46 @@ using SkyShoot.Contracts.Session;
 using SkyShoot.Contracts.Mobs;
 using Microsoft.Xna.Framework;
 using SkyShoot.Contracts.Service;
+using SkyShoot.Contracts.Weapon.Projectiles;
 
 namespace SkyShoot.Service.Session
 {
     public class GameSession
     {
 		private GameLevel _gameLevel;
-		private List<AMob> Mobs;
+		public List<AMob> players{get; set;}
+        public List<AMob> mobs{get; set;}
+        public List<AProjectile> projectiles { get; set; }
+        private int _movementTime;
         private SpiderFactory _spiderMobFactory;
-
 
 
 		public GameDescription LocalGameDescription { get; private set; }
 
-		public GameSession(TileSet tileSet, List<MainSkyShootService> players, int maxPlayersAllowed, GameMode gameType, int gameID)
+		public GameSession(TileSet tileSet, List<MainSkyShootService> clients, int maxPlayersAllowed, GameMode gameType, int gameID)
         {
+            _movementTime = 1000 / 60;
+
 			_gameLevel = new GameLevel(tileSet);
 
-            Mobs = new List<AMob>();
-            Mobs.AddRange(players.ToArray()); // здесь возможен exception
+            players = new List<AMob>();
+            players.AddRange(clients.ToArray()); // здесь возможен exception
 
-            foreach (MainSkyShootService player in players)
+            var playerNames = new List<string>();
+
+            foreach (MainSkyShootService player in clients)
             {
                 this.SomebodyMoves += new SomebodyMovesHadler(player.MobMoved);
                 player.MeMoved += new SomebodyMovesHadler(SomebodyMoved);
+
+                playerNames.Add(player.Name);
             }
 
+
+
+			LocalGameDescription = new GameDescription(playerNames, maxPlayersAllowed, gameType, gameID);
             _spiderMobFactory= new SpiderFactory(_gameLevel, 100, 100, 100); // характеристики моба ИЗМЕНИТЬ!
-            // LocalGameDescription = new GameDescription(players, maxPlayersAllowed, gameType, gameID);
-            // @todo: получить список имен из players
+            
         }
 
         public event SomebodyMovesHadler SomebodyMoves;
@@ -47,5 +58,43 @@ namespace SkyShoot.Service.Session
             }
         }
 
+        // здесь будут производится обработка всех действий
+        public void update() 
+        {
+            
+            foreach(AMob mob in mobs){
+                // @TODO АИ мобов 
+                mob.Coordinates = ComputeMovement(mob);
+            }
+            foreach(AMob player in players)
+                player.Coordinates = ComputeMovement(player);
+
+            //TODO collision detection;
+
+            foreach (AProjectile projectile in projectiles)
+            {
+                if (projectile.Timer <= 0) projectiles.Remove(projectile);//тут скорее всего будет ошибка
+                projectile.Coordinates += projectile.Direction*projectile.Speed;
+                projectile.Timer--;
+            }
+        }
+       
+        public Vector2 ComputeMovement(AMob mob)
+		{
+			var realHeight=_gameLevel.levelHeight-mob.Radius;
+			var realWidth=_gameLevel.levelWidth-mob.Radius;
+			var newCoord = new Vector2(mob.Coordinates.X + mob.Speed * _movementTime * mob.RunVector.X, mob.Coordinates.Y + mob.Speed * _movementTime * mob.RunVector.Y);
+			if(Math.Abs(mob.Coordinates.X) <= realWidth)
+				newCoord.X=Math.Min(Math.Abs(newCoord.X), realWidth) * Math.Sign(newCoord.X);
+			else
+				newCoord.X=Math.Min(Math.Abs(newCoord.X), realWidth + _gameLevel.LEVELBORDER) * Math.Sign(newCoord.X);
+				
+			if (Math.Abs(mob.Coordinates.Y) <= realHeight)
+				newCoord.Y = Math.Min(Math.Abs(newCoord.Y), realHeight) * Math.Sign(newCoord.Y);
+			else
+				newCoord.Y = Math.Min(Math.Abs(newCoord.Y), realHeight + _gameLevel.LEVELBORDER) * Math.Sign(newCoord.Y);
+
+			return newCoord;
+		}
     }
 }
