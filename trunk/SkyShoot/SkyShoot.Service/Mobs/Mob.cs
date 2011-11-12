962bde43ef77eab54e269b05ad39620152afbad1
chuvashov.andrey@gmail.com <chuvashov.andrey@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-12 17:44:13 +0000

    factory fixed, increase health added

--- trunk/SkyShoot/SkyShoot.Service/Mobs/Mob.cs
+++ trunk/SkyShoot/SkyShoot.Service/Mobs/Mob.cs
@@ -8,12 +8,20 @@ namespace SkyShoot.Contracts.Mobs
 {
     public class Mob : AMob
     {
+        private const float _speed = 100; // заменить
+        private const float _radius = 100; // заменить
+        private static Double _health = 100; // заменить начальное здоровье
+
         public AMob targetPlayer { get; set; }
 
         public Mob()
         {
             IsPlayer = false;
             Id = new Guid();
+            HealthAmount = (int)_health;
+            _health *= 1.001; // увеличение здоровья каждого следующего на 0.1%
+            Speed = _speed;
+            Radius = _radius;
         }
 
         private void FindTarget(List<AMob> targetPlayers) 
