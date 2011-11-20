d43bf45d98d5ac97a4e00e66508701d609e21adc
graph.di5@gmail.com <graph.di5@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-20 20:16:30 +0000

    Service as a program added for simple debug

---
+++ trunk/SkyShoot/SkyShoot.Service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SkyShoot.ServProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var host = new ServiceHost(typeof(SkyShoot.Service.MainSkyShootService),
+		new Uri("net.tcp://localhost:8002"));
+
+			host.AddServiceEndpoint(typeof(SkyShoot.Contracts.Service.ISkyShootService),
+															new NetTcpBinding(SecurityMode.None), "SkyShootService");
+
+			var metadataBehavior =
+					host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+			if (metadataBehavior == null)
+			{
+				metadataBehavior = new ServiceMetadataBehavior();
+				metadataBehavior.HttpGetEnabled = false;
+				host.Description.Behaviors.Add(metadataBehavior);
+			}
+
+			host.AddServiceEndpoint(typeof(IMetadataExchange),
+					MetadataExchangeBindings.CreateMexTcpBinding(),
+					"mex");
+
+			host.Open();
+			Console.WriteLine("Started!");
+			Console.ReadKey();
+			host.Close();
+
+		}
+	}
+}
