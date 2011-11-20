7343aa436262100cc44f2afe3025fc33a6e7bd7d
chuvashov.andrey@gmail.com <chuvashov.andrey@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-20 16:03:28 +0000

    Spider class added.

---
+++ SkyShoot/SkyShoot.Service/Mobs/Spider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkyShoot.Contracts.Mobs;
+
+namespace SkyShoot.Service.Mobs
+{
+    class Spider : Mob
+    {
+        public Spider() 
+        {
+            Radius = 100; // change to real value
+            Speed = 100; // change to real value
+        }
+    }
+}
