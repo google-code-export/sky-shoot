5ee3e38a1f3e0a3e1580c60ff22178b381939f6f
graph.di5@gmail.com <graph.di5@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-20 20:16:30 +0000

    Service as a program added for simple debug

---
+++ SkyShoot/SkyShoot.Service/Account/AccountInfoPrg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyShoot.Service.Account
+{
+	public class Account
+	{
+		public Account() : base() { }
+
+		//public Account(string partitionKey, string rowKey) : base(partitionKey, rowKey) { }
+
+		public string Login { get; set; }      // это rowKey в разделе partitionKey = "accounts"
+		public string Password { get; set; }
+		public string Email { get; set; }      // у приличных людей регистрация через эл.почту :)
+		public string Info { get; set; }       // здесь в дальнейшем будет храниться доп.информация о аккаунте (очки бонусов, номер кредитки :) и т.д)
+		// по идее нужно ещё добавить поле типа bool: true -- данный игрок сейчас залогинен и в игре, false -- игрок не в игре   
+	}
+}
+
---
+++ SkyShoot/SkyShoot.Service/Logger/TableTraceListenerProg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace SkyShoot.ServProgram.Logger
+{
+	class TableTraceListener : TraceListener
+	{
+		public override void Write(string message)
+		{
+			System.Console.Write("[" + DateTime.Now + "] INFO: " + message);
+		}
+
+		public override void WriteLine(string message)
+		{
+			System.Console.WriteLine("[" + DateTime.Now + "] INFO: " + message);
+		}
+
+		public override void Fail(string message)
+		{
+			System.Console.WriteLine("[" + DateTime.Now + "] ERROR: " + message);
+		}
+	}
+}
