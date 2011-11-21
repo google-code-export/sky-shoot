737d82e3b19c1d9dee89bb4235f2ce8dea1b5912
krasnovdmitrii@gmail.com <krasnovdmitrii@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-21 11:55:04 +0000

    Add WaitScreen

---
+++ SkyShoot/SkyShoot.Game/Screens/WaitScreen.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.Graphics;
+
+using Nuclex.UserInterface;
+
+using Nuclex.UserInterface.Controls;
+
+using Nuclex.UserInterface.Controls.Desktop;
+using SkyShoot.Contracts.Session;
+using SkyShoot.Game.Client.Game;
+using SkyShoot.Game.ScreenManager;
+
+namespace SkyShoot.Game.Screens
+{
+    internal class WaitScreen : GameScreen
+    {
+
+        private GuiManager _gui;
+        private Screen _mainScreen;
+        private LabelControl _playersLabel;
+        private ListControl _playersList;
+        private ButtonControl _leaveButton;
+        private LabelControl _tileLabel;
+        private LabelControl _gameModeLabel;
+        private LabelControl _maxPlayersLabel;
+        private string Tile;
+        private string GameMode;
+        private string MaxPlayers;
+
+        public WaitScreen(string tile, string gameMod, string maxPlayers)
+        {
+            this.Tile = tile;
+            this.GameMode = gameMod;
+            this.MaxPlayers = maxPlayers;
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            _gui = ScreenManager.Gui;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            _mainScreen = new Screen(viewport.Width, viewport.Height);
+            _gui.Screen = _mainScreen;
+
+            _mainScreen.Desktop.Bounds = new UniRectangle(
+                new UniScalar(0.1f, 0.0f), new UniScalar(0.1f, 0.0f),
+                new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f)
+                );
+
+            _playersLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(-60f, -34f, 100f, 24f),
+                Text = "Players"
+            };
+            _mainScreen.Desktop.Children.Add(_playersLabel);
+
+            _playersList = new ListControl
+            {
+                Bounds = new UniRectangle(-60f, -4f, 200f, 300f)
+            };
+            // todo список игроков
+            _playersList.Slider.Bounds.Location.X.Offset -= 1.0f;
+            _playersList.Slider.Bounds.Location.Y.Offset += 1.0f;
+            _playersList.Slider.Bounds.Size.Y.Offset -= 2.0f;
+            _playersList.SelectionMode = ListSelectionMode.Single;
+            _playersList.SelectedItems.Add(4);
+            _mainScreen.Desktop.Children.Add(_playersList);
+
+            // Leave Button
+            _leaveButton = new ButtonControl
+            {
+                Text = "Leave",
+                Bounds = new UniRectangle(new UniScalar(0.5f, -378f), new UniScalar(0.4f, 180f), 120, 32)
+            };
+            _leaveButton.Pressed += LeaveButtonPressed;
+            _mainScreen.Desktop.Children.Add(_leaveButton);
+
+            // информация о игре
+            // Tile label
+            _maxPlayersLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(200f, 200f, 100f, 24f),
+                Text = "Map :"
+            };
+            _mainScreen.Desktop.Children.Add(_maxPlayersLabel);
+
+            _maxPlayersLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(320f, 200f, 100f, 24f),
+                Text = Tile
+            };
+            _mainScreen.Desktop.Children.Add(_maxPlayersLabel);
+
+            // GameMode label
+            _gameModeLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(200f, 234f, 100f, 24f),
+                Text = "Game Mode :"
+            };
+            _mainScreen.Desktop.Children.Add(_gameModeLabel);
+
+            _gameModeLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(320f, 234f, 100f, 24f),
+                Text = GameMode
+            };
+            _mainScreen.Desktop.Children.Add(_gameModeLabel);
+
+            // Max Players label
+            _maxPlayersLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(200f, 268f, 100f, 24f),
+                Text = "Max Players :"
+            };
+            _mainScreen.Desktop.Children.Add(_maxPlayersLabel);
+
+            _maxPlayersLabel = new LabelControl
+            {
+                Bounds = new UniRectangle(320f, 268f, 100f, 24f),
+                Text = MaxPlayers
+            };
+            _mainScreen.Desktop.Children.Add(_maxPlayersLabel);
+            
+        }
+
+        private void LeaveButtonPressed(object sender, EventArgs args)
+        {  
+            GameController.Instance.LeaveGame();
+            ExitScreen();
+            ScreenManager.AddScreen(new MultiplayerScreen());
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            _gui.Draw(gameTime);
+        }
+
+    }
+
+}
