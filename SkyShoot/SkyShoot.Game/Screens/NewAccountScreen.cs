76cd60341fdb053123769de492d2d7ac7e779eaf
baragon92@gmail.com <baragon92@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-21 15:55:14 +0000

    Registration screen added.

--- SkyShoot/SkyShoot.Game/Screens/NewAccountScreen.cs
+++ SkyShoot/SkyShoot.Game/Screens/NewAccountScreen.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Nuclex.UserInterface;
-
 using Nuclex.UserInterface.Controls;
-
+using SkyShoot.Game.ScreenManager;
 using Nuclex.UserInterface.Controls.Desktop;
-
-using Microsoft.Xna.Framework;
-
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using SkyShoot.Game.Client.Game;
-
 
 namespace SkyShoot.Game.Screens
 {
-    class LoginScreen:ScreenManager.GameScreen
+    class NewAccountScreen:GameScreen
     {
         private GuiManager _gui;
         private LabelControl _loginLabel;
@@ -22,8 +20,7 @@ namespace SkyShoot.Game.Screens
         private LabelControl _passwordLabel;
         private InputControl _passwordBox;
         private ButtonControl _backButton;
-        private ButtonControl _loginButton;
-        private ButtonControl _newAccountButton;
+        private ButtonControl _okButton;
         private Screen _mainScreen;
 
         public override void LoadContent()
@@ -36,14 +33,14 @@ namespace SkyShoot.Game.Screens
 
             _mainScreen.Desktop.Bounds = new UniRectangle(
                 new UniScalar(0.1f, 0.0f), new UniScalar(0.1f, 0.0f),
-                new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f) 
+                new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f)
             );
 
             // Login Input
             _loginBox = new InputControl
             {
                 Bounds = new UniRectangle(new UniScalar(0.5f, -100f), new UniScalar(0.4f, -30), 200, 30),
-                Text = Settings.Default.login 
+                Text = ""
             };
             _mainScreen.Desktop.Children.Add(_loginBox);
 
@@ -51,7 +48,7 @@ namespace SkyShoot.Game.Screens
             _passwordBox = new InputControl
             {
                 Bounds = new UniRectangle(new UniScalar(0.5f, -100f), new UniScalar(0.4f, 30), 200, 30),
-                Text = Settings.Default.password
+                Text = ""
             };
             _mainScreen.Desktop.Children.Add(_passwordBox);
 
@@ -61,22 +58,13 @@ namespace SkyShoot.Game.Screens
                 Bounds = new UniRectangle(new UniScalar(0.5f, -32), new UniScalar(0.4f, -70), 100, 30)
             };
             _mainScreen.Desktop.Children.Add(_loginLabel);
-            
+
             // Password Label
             _passwordLabel = new LabelControl("Password")
             {
                 Bounds = new UniRectangle(new UniScalar(0.5f, -32), new UniScalar(0.4f, 0), 100, 30)
             };
             _mainScreen.Desktop.Children.Add(_passwordLabel);
-
-            // Login Button
-            _loginButton = new ButtonControl
-            {
-                Text = "Login",
-                Bounds = new UniRectangle(new UniScalar(0.5f, 110), new UniScalar(0.4f, 70), 100, 32)
-            };
-            _loginButton.Pressed += LoginButtonPressed;
-            _mainScreen.Desktop.Children.Add(_loginButton);
 
             // Back Button
             _backButton = new ButtonControl
@@ -87,14 +75,14 @@ namespace SkyShoot.Game.Screens
             _backButton.Pressed += BackButtonPressed;
             _mainScreen.Desktop.Children.Add(_backButton);
 
-            // New Account Button
-            _newAccountButton = new ButtonControl
+            // Login Button
+            _okButton = new ButtonControl
             {
-                Text = "Create new account",
-                Bounds = new UniRectangle(new UniScalar(0.5f, -75f), new UniScalar(0.4f, 70), 150, 32)
+                Text = "OK",
+                Bounds = new UniRectangle(new UniScalar(0.5f, 110), new UniScalar(0.4f, 70), 100, 32)
             };
-            _newAccountButton.Pressed += NewAccountButtonPressed;
-            _mainScreen.Desktop.Children.Add(_newAccountButton);
+            _okButton.Pressed += OkButtonPressed;
+            _mainScreen.Desktop.Children.Add(_okButton);
 
         }
 
@@ -103,10 +91,9 @@ namespace SkyShoot.Game.Screens
             ExitScreen();
         }
 
-        private void LoginButtonPressed(object sender, EventArgs args)
+        private void OkButtonPressed(object sender, EventArgs args)
         {
-
-            if(_loginBox.Text.Length < 3) ScreenManager.AddScreen(new MessageBox("Username is too short!\nPress Enter to continue"));
+            if (_loginBox.Text.Length < 3) ScreenManager.AddScreen(new MessageBox("Username is too short!\nPress Enter to continue"));
             else if (_passwordBox.Text.Length < 3) ScreenManager.AddScreen(new MessageBox("Password is too short!\nPress Enter to continue"));
             else
             {
@@ -114,17 +101,13 @@ namespace SkyShoot.Game.Screens
                 Settings.Default.password = _passwordBox.Text;
                 Settings.Default.Save();
 
-                GameController.Instance.Login(_loginBox.Text, _passwordBox.Text);
+                GameController.Instance.Register(_loginBox.Text, _passwordBox.Text);
 
                 ScreenManager.AddScreen(new MultiplayerScreen());
             }
+            ExitScreen();
         }
 
-        private void NewAccountButtonPressed(object sender, EventArgs args)
-        {
-            ScreenManager.AddScreen(new NewAccountScreen());
-            //ExitScreen();
-        }
 
         public override void Draw(GameTime gameTime)
         {
