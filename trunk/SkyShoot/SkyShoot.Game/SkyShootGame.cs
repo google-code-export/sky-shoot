6bf5a57cff14b8d1c3142178df9fc33f938532bf
stanislav.sartasov@gmail.com <stanislav.sartasov@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-22 18:57:48 +0000

    - reverted previous commit because it was for info purposes only

--- trunk/SkyShoot/SkyShoot.Game/SkyShootGame.cs
+++ trunk/SkyShoot/SkyShoot.Game/SkyShootGame.cs
@@ -1,17 +1,11 @@
 using Microsoft.Xna.Framework;
 using SkyShoot.Game.Client.View;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace SkyShoot.Game
 {
     public class SkyShootGame : Microsoft.Xna.Framework.Game
     {
         private ScreenManager.ScreenManager _screenManager;
-        Texture2D cursorTexture;
-        Vector2 cursorPosition;
-        SpriteBatch spriteBatch;
-
         public SkyShootGame()
         {
             new GraphicsDeviceManager(this);
@@ -34,8 +28,7 @@ namespace SkyShoot.Game
 
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            cursorTexture = Content.Load<Texture2D>("Textures/Cursors/Arrow");
+
         }
 
         protected override void UnloadContent()
@@ -43,19 +36,9 @@ namespace SkyShoot.Game
 
         }
 
-        protected override void Update(GameTime gameTime)
-        {
-            cursorPosition.X = Mouse.GetState().X;
-            cursorPosition.Y = Mouse.GetState().Y;
-            base.Update(gameTime);
-        }
-
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-            spriteBatch.Begin(SpriteSortMode.Texture, BlendState.AlphaBlend);
-            spriteBatch.Draw(cursorTexture, cursorPosition, Color.White);
-            spriteBatch.End();
             base.Draw(gameTime);
 
         }
