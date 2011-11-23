fcd175c8b7922411ca1d352b2e39ffbd848b5b32
Sergey.Tereshchenko.spbgu@gmail.com <Sergey.Tereshchenko.spbgu@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-23 02:33:01 +0000

    modified hash(md5)-password

--- SkyShoot/SkyShoot.Service/Account/AccountManager.cs
+++ SkyShoot/SkyShoot.Service/Account/AccountManager.cs
@@ -4,12 +4,12 @@ namespace SkyShoot.Service.Account
 {
     public class AccountManager
     {
-/**
-        (!) нужно будет делать регистрацию приводя все знаки username / password к строчным
-        ещё проверять нужно будет пароли и имена, чтобы они содержали только a-z,A-Z,1-9 ,-,_ и никаких пробелов
+        /**
+                (!) нужно будет делать регистрацию приводя все знаки username / password к строчным
+                ещё проверять нужно будет пароли и имена, чтобы они содержали только a-z,A-Z,1-9 ,-,_ и никаких пробелов
           
-        Пароль в эти функции уже должен приходить как md5 хеш, это делается при вызове функций на клиенте...только где это писать
-        **/
+                Пароль в эти функции уже должен приходить как md5 хеш, это делается при вызове функций на клиенте...только где это писать
+                **/
 
         public bool Register(string user_name, string password)
         {
@@ -18,8 +18,15 @@ namespace SkyShoot.Service.Account
                 configSetter(RoleEnvironment.GetConfigurationSettingValue(configName));
             });
 
+            string salt = HashHelper.GetRandomString();
             AccountManagerEntry entry = new AccountManagerEntry()
-                    {Account = user_name, HashPassword = password, Salt = HashHelper.GetRandomString(), Email = "sky@shoot", Info = "--" };
+            {
+                Account = user_name,
+                HashPassword = HashHelper.GetMd5Hash(password + salt),
+                Salt = salt,
+                Email = "sky@shoot",
+                Info = "--"
+            };
             AccountManagerDataSource ds = new AccountManagerDataSource();
             return ds.AddAccountManagerEntry(entry);
         }
