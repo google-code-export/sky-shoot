ddcc88a225553d93e3a0e1e07c2e80dd19a4506f
FWFKirillPopov@gmail.com <FWFKirillPopov@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-24 20:47:08 +0000

    Players speed changed to 0.025f. It`s equal 25pixels per second. Removed Constants.FPS from the newCord calculations.

--- trunk/SkyShoot/SkyShoot.Contracts/Service/Constants.cs
+++ trunk/SkyShoot/SkyShoot.Contracts/Service/Constants.cs
@@ -7,7 +7,7 @@ namespace SkyShoot.Contracts
 {
 	public static class Constants
 	{
-		public const float PLAYER_SPEED = 0.5f;
+		public const float PLAYER_SPEED = 0.025f;
 		public const float PLAYER_RADIUS = 15f;
 		public const int FPS = 1000 / 60;
 	}
