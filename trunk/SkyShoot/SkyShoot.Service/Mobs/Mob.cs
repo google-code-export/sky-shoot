061a71a1157986efad39becc0ad2483ed15af778
chuvashov.andrey@gmail.com <chuvashov.andrey@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-29 15:49:33 +0000

    bug fixed

--- trunk/SkyShoot/SkyShoot.Service/Mobs/Mob.cs
+++ trunk/SkyShoot/SkyShoot.Service/Mobs/Mob.cs
@@ -10,74 +10,77 @@ using SkyShoot.Contracts.Weapon.Projectiles;
 
 namespace SkyShoot.Contracts.Mobs
 {
-    public class Mob : AMob
-    {
-        private static Double _health = 100; //change to real value 
+	public class Mob : AMob
+	{
+		private static Double _health = 100; //change to real value 
 
 
-        public MainSkyShootService targetPlayer { get; set; }
+		public MainSkyShootService targetPlayer { get; set; }
 
-        public float Damage { get; set; }
+		public float Damage { get; set; }
 
-        private int _counter;
+		private int _counter;
 
-        public Mob()
-        {
-            IsPlayer = false;
-            _counter = 0;
-            Id = Guid.NewGuid();
-            HealthAmount = (float)_health;
-            Damage = 10;
-            _health *= 1.001; // увеличение здоровья каждого следующего на 0.1%
-        }
+		public Mob()
+		{
+			IsPlayer = false;
+			_counter = 0;
+			Id = Guid.NewGuid();
+			HealthAmount = (float)_health;
+			Damage = 10;
+			_health *= 1.001; // увеличение здоровья каждого следующего на 0.1%
+		}
 
-        public void FindTarget(List<MainSkyShootService> targetPlayers) 
-        {
-            float distance = 1000000;
-            float temp;
+		public void FindTarget(List<MainSkyShootService> targetPlayers)
+		{
+			float distance = 1000000;
+			float temp;
 
-            foreach (MainSkyShootService player in targetPlayers)
-            {
-                temp = Vector2.Distance(Coordinates, player.Coordinates);
+			foreach (MainSkyShootService player in targetPlayers)
+			{
+				temp = Vector2.Distance(Coordinates, player.Coordinates);
 
-                if (distance > temp)
-                {
-                    distance = temp;
-                    targetPlayer = player;
-                }
-            }
-        }
-
-        public event SomebodyMovesHandler MeMoved;
-
-				public virtual void Move()
+				if (distance > temp)
 				{
-					RunVector = new Vector2(targetPlayer.Coordinates.X - Coordinates.X, targetPlayer.Coordinates.Y - Coordinates.Y);
-                    RunVector = Vector2.Normalize(RunVector);
-					ShootVector = RunVector;
-
-					if (MeMoved != null)
-					{
-						System.Diagnostics.Trace.WriteLine("Mob run vector" + RunVector);
-						MeMoved(this, RunVector);
-					}
+					distance = temp;
+					targetPlayer = player;
 				}
+			}
+		}
 
-        public virtual void Think(List<MainSkyShootService> players)
-        {
-            if (_counter % 20 == 0)
-            {
-                if (!players.Contains(targetPlayer) || targetPlayer == null)
-                {
-                    FindTarget(players);
-                }
-            }
-            _counter++;
-        }
+		public event SomebodyMovesHandler MeMoved;
 
-        public void DamageTaken(AProjectile bullet)
-        {
-            HealthAmount -= bullet.Damage;
-        }
-    }
+		public virtual void Move()
+		{
+			RunVector = new Vector2(targetPlayer.Coordinates.X - Coordinates.X, targetPlayer.Coordinates.Y - Coordinates.Y);
+			RunVector = Vector2.Normalize(RunVector);
+			ShootVector = RunVector;
+
+			/*
+			if (MeMoved != null)
+			{
+				System.Diagnostics.Trace.WriteLine("Mob run vector" + RunVector);
+				MeMoved(this, RunVector);
+			}
+			*/
+		}
+
+		public virtual void Think(List<MainSkyShootService> players)
+		{
+			if (_counter % 20 == 0)
+			{
+				if (!players.Contains(targetPlayer) || targetPlayer == null)
+				{
+					FindTarget(players);
+				}
+				Move();
+			}
+			_counter++;
+		}
+
+		public void DamageTaken(AProjectile bullet)
+		{
+			HealthAmount -= bullet.Damage;
+		}
+	}
 }
