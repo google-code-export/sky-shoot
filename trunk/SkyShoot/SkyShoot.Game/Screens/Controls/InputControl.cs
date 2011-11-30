545e882d3e84b9efad07f0cfec660838ffae9142
baragon92@gmail.com <baragon92@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2011-11-30 18:02:37 +0000

    Some changes in LoginScreen

---
+++ trunk/SkyShoot/SkyShoot.Game/Screens/Controls/InputControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyShoot.Game.Screens.Controls
+{
+    class InputControl:Nuclex.UserInterface.Controls.Desktop.InputControl
+    {
+        protected override void OnCharacterEntered(char character)
+        {
+            if (Char.IsLetter(character) || Char.IsDigit(character))
+            {
+                Text += character;
+                CaretPosition += 1;
+            }
+        }
+    }
+}
