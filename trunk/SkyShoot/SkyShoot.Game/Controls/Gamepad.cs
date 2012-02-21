c25e3f1b0891761b8673af83a0343a68ac8ad396
F.Pavel91@gmail.com <F.Pavel91@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-02-21 09:27:20 +0000

    Refactoring

--- trunk/SkyShoot/SkyShoot.Game/Controls/Gamepad.cs
+++ trunk/SkyShoot/SkyShoot.Game/Controls/Gamepad.cs
@@ -1,19 +1,49 @@
 ﻿using System;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Nuclex.Input;
 using Nuclex.UserInterface.Controls;
 
 namespace SkyShoot.Game.Controls
 {
 	class Gamepad : Controller
 	{
-		public void Update()
+		private GamePadState _currentGamePadState;
+		private GamePadState _lastGamePadState;
+
+		public Gamepad(InputManager inputManager) : base(inputManager)
 		{
-			
+			// todo windows | XBOX
+			_currentGamePadState = InputManager.GetGamePad(ExtendedPlayerIndex.Five).GetState();
 		}
 
-		public void RegisterListener(PressableControl control, EventHandler e)
+		public override void Update()
 		{
-			
+			// todo wrong, 10%
+			_lastGamePadState = _currentGamePadState;
+
+			_currentGamePadState = InputManager.GetGamePad(ExtendedPlayerIndex.Five).GetState();			
+		}
+
+		public override Vector2 RunVector
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override Vector2 SightPosition
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		private Vector2 GetRunVector(GamePadState gamePadState)
+		{
+			Vector2 runVector = Vector2.Zero;
+			if (gamePadState.IsConnected)
+			{
+				runVector = gamePadState.ThumbSticks.Left;
+				runVector.Y = -runVector.Y;
+			}
+			return runVector;
 		}
 	}
 }
---
+++ trunk/SkyShoot/SkyShoot.Game/Controls/KeyboardAndMouse.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Nuclex.Input;
+
+namespace SkyShoot.Game.Controls
+{
+	class KeyboardAndMouse : Controller
+	{
+		private KeyboardState _currentKeyboardState;
+		private KeyboardState _lastKeyState;
+
+		private MouseState _currentMouseState;
+		private MouseState _lastMouseState;
+
+		public KeyboardAndMouse(InputManager inputManager) : base(inputManager)
+		{
+			_currentKeyboardState = InputManager.GetKeyboard().GetState();
+			_currentMouseState = InputManager.GetMouse().GetState();
+		}
+
+		public override void Update()
+		{
+			_lastKeyState = _currentKeyboardState;
+			_lastMouseState = _currentMouseState;
+
+			_currentKeyboardState = InputManager.GetKeyboard().GetState();
+			_currentMouseState = InputManager.GetMouse().GetState();
+		}
+
+		public override Vector2 RunVector
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override Vector2 SightPosition
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		private Vector2 GetRunVector(KeyboardState keyboardState)
+		{
+			Vector2 runVector = Vector2.Zero;
+			switch (Settings.Default.KeyboardLayout)
+			{
+				case 0:
+					{
+						if (keyboardState.IsKeyDown(Keys.W)) runVector -= Vector2.UnitY;
+						if (keyboardState.IsKeyDown(Keys.S)) runVector += Vector2.UnitY;
+						if (keyboardState.IsKeyDown(Keys.A)) runVector -= Vector2.UnitX;
+						if (keyboardState.IsKeyDown(Keys.D)) runVector += Vector2.UnitX;
+					}
+					break;
+				case 1:
+					{
+						if (keyboardState.IsKeyDown(Keys.Up)) runVector -= Vector2.UnitY;
+						if (keyboardState.IsKeyDown(Keys.Down)) runVector += Vector2.UnitY;
+						if (keyboardState.IsKeyDown(Keys.Left)) runVector -= Vector2.UnitX;
+						if (keyboardState.IsKeyDown(Keys.Right)) runVector += Vector2.UnitX;
+					}
+					break;
+			}
+			if (runVector.Length() > 0)
+				runVector.Normalize();
+			return runVector;
+		}
+	}
+}
