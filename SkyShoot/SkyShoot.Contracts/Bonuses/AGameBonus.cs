6fbb2172cb53a163223ce7d0c26ab9d379043827
mikhaylova.alexandra.a <mikhaylova.alexandra.a@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-03-19 21:53:06 +0000

    Working on bonuses (shield, double damage)

--- SkyShoot/SkyShoot.Contracts/Bonuses/AGameBonus.cs
+++ SkyShoot/SkyShoot.Contracts/Bonuses/AGameBonus.cs
@@ -9,6 +9,14 @@ namespace SkyShoot.Contracts.Bonuses
 {
 	public class AGameBonus : AGameObject
 	{
+		private int _milliseconds; // @Sergey Terechenko : time = health
+		private DateTime _startTime;
+
 		public AGameBonus(Vector2 coordinates) : base(coordinates, Guid.NewGuid()) { }
-	}
+
+		public bool IsExpired(DateTime time)
+		{
+			return (time.CompareTo(_startTime.AddMilliseconds(_milliseconds)) == 1);
+		}
+	}               
 }
