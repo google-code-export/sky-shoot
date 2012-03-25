8cf2c183ed22d94662f2b7aa81d4dabf9f6ad9f1
mikhaylova.alexandra.a <mikhaylova.alexandra.a@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-03-25 21:35:40 +0000

    Bonuses done

--- SkyShoot/SkyShoot.Service/Bonus/BonusFactory.cs
+++ SkyShoot/SkyShoot.Service/Bonus/BonusFactory.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SkyShoot.Contracts.Bonuses;
+using SkyShoot.XNA.Framework;
 
-namespace SkyShoot.ServProgram.Bonus
+namespace SkyShoot.Service.Bonus
 {
 	class BonusFactory
 	{
+		private Random _random = new Random();
+
+		public AGameBonus CreateBonus(Vector2 coordinates)
+		{
+			if (_random.Next(2) == 0)
+			{
+				return new Shield(coordinates);
+			}
+			else
+			{
+				return new DoubleDamage(coordinates);
+			}
+		}
 	}
 }
