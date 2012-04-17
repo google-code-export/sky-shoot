f1c896922c0e49662ff7ae4c9b7626b88e12aefc
graph.di5@gmail.com <graph.di5@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-04-17 15:01:24 +0000

    added WeaponButton.cs

---
+++ SkyShoot/Skyshoot.WinformsClient/WeaponButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using SkyShoot.Contracts.Weapon;
+
+namespace SkyShoot.WinFormsClient
+{
+	class WeaponButton : Label
+	{
+		public AWeapon.AWeaponType WeaponType
+		{
+			get;
+			set;
+		}
+	}
+}
