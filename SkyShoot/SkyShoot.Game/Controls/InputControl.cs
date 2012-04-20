7758ff91f6c79b118ebb3f36828e190132823857
JuliaBaic@gmail.com <JuliaBaic@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-04-20 09:18:07 +0000

    hidden password

--- SkyShoot/SkyShoot.Game/Controls/InputControl.cs
+++ SkyShoot/SkyShoot.Game/Controls/InputControl.cs
@@ -4,11 +4,32 @@ namespace SkyShoot.Game.Controls
 {
 	internal class InputControl : Nuclex.UserInterface.Controls.Desktop.InputControl
 	{
+		public Boolean IsHidden
+		{
+			get;
+			set;
+		}
+
+		public String RealText
+		{
+			get;
+			set;
+		}
+
+		public static string HiddenText (string text)
+		{			
+			string _newString = "";
+			for (int i = 1; i <= text.Length; i++) _newString += "*";
+			return _newString;
+		}
+
 		protected override void OnCharacterEntered(char character)
 		{
 			if (Char.IsLetter(character) || Char.IsDigit(character) || (character == '_'))
 			{
-				Text += character;
+				RealText += character;
+				if (IsHidden) Text += "*";
+				else Text += character;
 				CaretPosition += 1;
 			}
 		}
