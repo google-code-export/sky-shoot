d0be27f538807eafc27a82caa650055fc0ce93b7
graph.di5@gmail.com <graph.di5@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-04-27 15:10:18 +0000

    small fix of misprint

--- trunk/SkyShoot/SkyShoot.Contracts/Service/Constants.cs
+++ trunk/SkyShoot/SkyShoot.Contracts/Service/Constants.cs
@@ -48,7 +48,7 @@ namespace SkyShoot.Contracts
 		public const float HEATER_BULLET_SPEED = 0.5f;
 		public const float HEATER_BULLET_DAMAGE = 100;
 		public const float HEATER_BULLET_LIFE_DISTANCE = 3000;
-		public const int HEATER_ATTACK_RATE = 400;
+		public const int HEATER_ATTACK_RATE = 2000;
 
 		#endregion
 
