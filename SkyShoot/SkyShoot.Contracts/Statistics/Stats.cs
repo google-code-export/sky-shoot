7f1d2a4ef1becbbb112242332e6312bd31f31328
gordeev92@gmail.com <gordeev92@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-07-04 13:21:24 +0000

    Added missing files

---
+++ SkyShoot/SkyShoot.Contracts/Statistics/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyShoot.Contracts.Statistics
+{
+	public struct Stats
+	{
+		public int Exp;
+		public int Frag;
+		public int Lvl;
+	}
+
+}
