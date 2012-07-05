0e3a8c95192d296cc85ffa482f789fd4b0d879b5
without.memory <without.memory@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-07-05 13:40:59 +0000

    Вместо возни с List ради передачи списка команд написал класс PlayerList.

--- trunk/SkyShoot/SkyShoot.Contracts/Service/Commands.cs
+++ trunk/SkyShoot/SkyShoot.Contracts/Service/Commands.cs
@@ -3,11 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SkyShoot.Contracts.Service
+namespace SkyShoot.Contracts.Service.Commands
 {
 	class Player
 	{
-		int command;
-		string name;
+		public int command;
+		public string name;
+	}
+
+	class PlayerList
+	{
+		public List<Player> Players = new List<Player>();
+
+		public void AddPlayer(string name)
+		{
+			Player newPlayer = new Player();
+			newPlayer.name = name;
+			newPlayer.command = 1; //По умолчанию
+			Players.Add(newPlayer);
+		}
+
+		public void AddPlayer(Player player)
+		{
+			Players.Add(player);
+		}
+
+		private Player FindPlayer(string name)
+		{
+			var found = Players.Find(p => p.name.Equals(name));
+			return found;
+		}
+
+		public void RemovePlayer(string name)
+		{
+			Players.Remove(FindPlayer(name));
+		}
+		
+		public void ChangePlayerTeam(string name)
+		{
+			FindPlayer(name).name = name;
+		}
+
+		public int PlayerTeam(string name)
+		{
+			return FindPlayer(name).command;
+		}
 	}
 }
+
