4b242d6c5bc6b90366f13081809985c3632fc0a8
georg992@gmail.com <georg992@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-07-09 13:15:00 +0000

    AccountManagerErrorCode enum added

---
+++ SkyShoot/SkyShoot.Contracts/Service/AccountManagerErrorCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyShoot.Contracts.Service
+{
+	public enum AccountManagerErrorCode
+	{
+		Ok,
+		UnknownExceptionOccured,
+		InvalidUsernameOrPassword,
+		UsernameTaken,
+		UnknownError //if this value is returned, then AccountManager code must be bugged
+	}
+}
