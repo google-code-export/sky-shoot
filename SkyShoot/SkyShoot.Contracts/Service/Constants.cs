ec255f27b106d694e391064ddb8567784d54674a
without.memory <without.memory@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-07-12 08:44:33 +0000

    Подправил константы, из отравляющего моба больше не сыпятся бонусы.

--- SkyShoot/SkyShoot.Contracts/Service/Constants.cs
+++ SkyShoot/SkyShoot.Contracts/Service/Constants.cs
@@ -84,11 +84,11 @@ namespace SkyShoot.Contracts
 
 		#region poisonTick
 
-		public const float POISONTICK_BULLET_SPEED = 0.5f;
-		public const float POISONTICK_BULLET_DAMAGE = 15f;
+		public const float POISONTICK_BULLET_SPEED = 1.5f;
+		public const float POISONTICK_BULLET_DAMAGE = 1.0f;
 		public const float POISONTICK_BULLET_LIFE_DISTANCE = 15f;
 		public const float POISONTICK_BULLET_RADIUS = 5f;
-		public const int POISONTICK_ATTACK_RATE = 1000;
+		public const int POISONTICK_ATTACK_RATE = 2000;
 
 		#endregion
 
