5c3a3c51b4e2f0ba38258dce3e20e76587e70645
georg992@gmail.com <georg992@gmail.com@4f6f678c-2229-9214-ea69-8f4d02484265>
2012-07-21 15:35:33 +0000

    Добавлены методы ApplyEvents и ExtrapolateTo

--- SkyShoot/SkyShoot.Contracts/SynchroFrames/SynchroFrame.cs
+++ SkyShoot/SkyShoot.Contracts/SynchroFrames/SynchroFrame.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using SkyShoot.Contracts.GameObject;
+using SkyShoot.Contracts.GameEvents;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using SkyShoot.Contracts.Session;
 
 namespace SkyShoot.Contracts.SynchroFrames
 {
@@ -14,7 +18,9 @@ namespace SkyShoot.Contracts.SynchroFrames
 		/// в момент времени Time
 		/// </summary>
 		[DataMember]
-		private readonly AGameObject[] _gameObjects;
+		public ConcurrentDictionary<Guid, AGameObject> _gameObjects;
+
+		private GameLevel _gameLevel;
 
 		/// <summary>
 		/// Время с начала игры на сервере, к которому относится синхрокадр
@@ -22,15 +28,20 @@ namespace SkyShoot.Contracts.SynchroFrames
 		[DataMember]
 		public long Time { get; private set; }
 
-		public AGameObject this[int index]
+		public AGameObject this[Guid index]
 		{
 			get { return _gameObjects[index]; }
 		}
 
-		public SynchroFrame(AGameObject[] gameObjects, long time)
+		public SynchroFrame(AGameObject[] gameObjects, long time/*, GameLevel gameLevel*/)
 		{
-			_gameObjects = gameObjects;
+			//_gameObjects = gameObjects;
+			foreach (var gameObject in gameObjects)
+			{
+				_gameObjects.TryAdd(gameObject.Id, gameObject);
+			}
 			Time = time;
+			//_gameLevel = gameLevel;
 		}
 
 		IEnumerator<AGameObject> IEnumerable<AGameObject>.GetEnumerator()
@@ -48,9 +59,34 @@ namespace SkyShoot.Contracts.SynchroFrames
 			return String.Format("SynchroFrame, Time = {0}", Time);
 		}
 
-//		public AGameObject[] ExtrapolateTo(GameTime gameTime)
-//		{
-//			
-//		}
+		public void ApplyEvents(AGameEvent[] gameEvents)
+		{
+			long timeSinceLastEvent;
+			foreach (var gameEvent in gameEvents) {
+				if (gameEvent.Type == EventType.NewObjectEvent)
+				{
+					var newGameObject = new AGameObject();
+					gameEvent.UpdateMob(newGameObject);
+
+					//DrawableGameObject newDrawableGameObject = GameFactory.CreateClientGameObject(newGameObject);
+					//AddGameObject(newDrawableGameObject);
+				}
+				timeSinceLastEvent = gameEvent.TimeStamp - Time;
+				Debug.Assert(timeSinceLastEvent >= 0);
+				_gameObjects[gameEvent.GameObjectId.Value].Coordinates = _gameObjects[gameEvent.GameObjectId.Value].ComputeMovement(timeSinceLastEvent, _gameLevel); // TODO: чтобы работало с новым ComputeMovement
+			}
+		}
+
+		public AGameObject[] ExtrapolateTo(long gameTime)
+		{
+			AGameObject[] result = new AGameObject[_gameObjects.Count];
+			int index = 0;
+			foreach (var gameObject in _gameObjects)
+			{
+				result[index].Copy(gameObject.Value);
+				result[index].Coordinates = result[index].ComputeMovement(gameTime - Time, _gameLevel); // TODO: чтобы работало с новым ComputeMovement
+			}
+			return result;
+		}
 	}
 }
